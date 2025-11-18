72313e9ac95dcf5ac2212b61a3dcd1f74eef25fd
김한성 <hsung0714@gmail.com>
2025-11-19 00:15:53 +0900

    Feat: 유니티로 돌아가는 버튼 생성

--- src/page/RedPage.tsx
+++ src/page/RedPage.tsx
@@ -2,12 +2,16 @@ import styles from "./RedPage.module.css";
 import AttendanceGrid from "../component/RedPageCommponent/AttendanceGrid";
 import BuildingGrid from "../component/RedPageCommponent/BuildingGrid";
 import Title from "../layout/Title";
+import GameComponent from "../component/GameComponent";
 
 const RedPage = () => {
   return (
     <section className={styles.wrapper}>
       <div className={styles.titleWrapper}>
         <Title />
+      </div>
+      <div className={styles.BackToGame}>
+        <GameComponent />
       </div>
       <div className={styles.content}>
         <AttendanceGrid />
