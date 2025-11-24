3b7e1b6e332adbc902657273ccc4a2365e92e984
김한성 <hsung0714@gmail.com>
2025-11-24 20:56:43 +0900

    Feat: 조이스틱 코드 수정(2)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import RedPage from "./page/RedPage";
 import BluePage from "./page/BluePage";
 import UpDownButton from "./commponent/UpDownButton";
-import enjoystick from "../enjoystick";
+import useJoystickNavigation from "../enjoystick";  // ← 경로 also fix
 
 const App = () => {
   const [isBluePage, setIsBluePage] = useState(true);
@@ -11,9 +11,13 @@ const App = () => {
   const handleTogglePage = () => {
     setIsBluePage((prev) => !prev);
   };
-  enjoystick(() => {
+
+  // 여기서 패드 입력 받음
+  useJoystickNavigation(() => {
     console.log("패드 입력 감지됨! (App.tsx)");
+    handleTogglePage(); // ← 페이지 토글도 바로 연결 가능
   });
+
   return (
     <>
       <div className="app-wrapper">
@@ -23,6 +27,7 @@ const App = () => {
         >
           {isBluePage ? <BluePage /> : <RedPage />}
         </div>
+
         <UpDownButton isBluePage={isBluePage} onToggle={handleTogglePage} />
       </div>
     </>
