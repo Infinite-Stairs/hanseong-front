6dbf6ea19474683a76363dd0ae4d2f7d97db7c66
김한성 <hsung0714@gmail.com>
2025-11-28 03:10:50 +0900

    Feat: 솔루션페이지 이미지 삽입

--- src/page/Balance_SolutionPage.tsx
+++ src/page/Balance_SolutionPage.tsx
@@ -1,14 +1,23 @@
 import styles from "./Balance_Solution.module.css";
+import 발근력 from "../assets/balance_Solution/발근력.png";
+import 전신정렬개선 from "../assets/balance_Solution/전신정렬개선.png";
+import 종아리스트레칭 from "../assets/balance_Solution/종아리스트레칭.png";
 
 const Balance_SolutionPage = () => {
-    return(
-<>
-<div className={styles.SolutionWarrap}>
-    <div className={styles.balance_solution}>
-        
-    </div>
-</div>
-</>
+    return (
+        <>
+            <div className={styles.SolutionWarrap}>
+                <div className={styles.first_pic}>
+                    <img src={발근력} alt="발근력사진" width={200} />
+                </div>
+                <div className={styles.sec_pic}>
+                    <img src={전신정렬개선} alt="전신정렬사진" width={200} />
+                </div>
+                <div className={styles.third_pic}>
+                    <img src={종아리스트레칭} alt="종아리스트래칭" width={200} />
+                </div>
+            </div>
+        </>
     );
 }
 
--- src/page/Cop_SolutionPage.tsx
+++ src/page/Cop_SolutionPage.tsx
@@ -1,10 +1,25 @@
 import styles from "./Cop_SolutionPage.module.css";
+import 고양이_낙타자세 from "../assets/Cop_Solution/고양이_낙타자세.png";
+import 버드독운동  from "../assets/Cop_Solution/버드독운동.png";
+import 브릿지 from "../assets/Cop_Solution/브릿지.png";
+import 앉기자세 from "../assets/Cop_Solution/앉기자세.png";
 
 const Cop_SolutionPage = () => {
     return(
 <>
 <div className={styles.SolutionWarrap}>
-
+        <div className={styles.first_pic}>
+            <img src={고양이_낙타자세} alt="고양이낙타자세" width={200} />
+        </div>
+        <div className={styles.sec_pic}>
+            <img src={버드독운동} alt="버드독운동" width={200} />
+        </div>
+        <div className={styles.third_pic}>
+            <img src={브릿지} alt="브릿지" width={200} />
+        </div>
+        <div className={styles.forth_pic}>
+            <img src={앉기자세} alt="브릿지" width={200} />
+        </div>
 </div>
 </>
     );
