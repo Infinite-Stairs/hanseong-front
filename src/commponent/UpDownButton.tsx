1230e302987356aca79cc3af5cfcf435bc7a8b76
김한성 <hsung0714@gmail.com>
2025-12-01 21:18:08 +0900

    Feat: 업다운버튼 조이스틱 코드 작성

--- src/commponent/UpDownButton.tsx
+++ src/commponent/UpDownButton.tsx
@@ -13,16 +13,18 @@ const UpDownButton = ({ isBluePage, onToggle }: UpDownButtonProps) => {
     <>
       {isBluePage ? (
         <div
-          className={styles.BlueButton}
+          className={`${styles.BlueButton} joystick-focus`}
           onClick={onToggle} // 파랑 클릭 → RedPage + 빨강버튼
+          tabIndex={0}
         >
           <img src={파랑} alt="파랑버튼" width={200} />
         </div>
 
       ) : (
         <div
-          className={styles.RedButton}
+          className={`${styles.RedButton} joystick-focus`}
           onClick={onToggle} // 빨강 클릭 → BluePage + 파랑버튼
+          tabIndex={0}
         >
           <img src={빨강} alt="빨강버튼" width={200} />
         </div>
